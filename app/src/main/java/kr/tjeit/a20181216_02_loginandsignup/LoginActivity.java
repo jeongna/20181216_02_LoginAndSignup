6427b08bd3ecd18c348ee2551affa17b282a96a7
ksh1101 <0solarboy0@naver.com>
2018-12-16 16:17:56 +0900

    로그인 기능 작성 - idEdt에 입력된 값을 받아서 intent에 첨부 - MainActivity에서 첨부된 String을 받아다 화면에 표시 - 액티비티간 데이터 송/수신

--- app/src/main/java/kr/tjeit/a20181216_02_loginandsignup/LoginActivity.java
+++ app/src/main/java/kr/tjeit/a20181216_02_loginandsignup/LoginActivity.java
@@ -4,11 +4,15 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class LoginActivity extends BaseActivity {
 
     TextView signUpTxt;
+    Button loginBtn;
+    EditText idEdt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,27 @@ public class LoginActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//               아이디 Edit에 뭐라고 젹혀있는지? 변수에 저장
+                String inputId = idEdt.getText().toString();
+
+
+//                메인액티비티로 넘어갈떄, inputId를 들고가려면?
+//                intent 변수에게 첨부물을 돌려주면 됨
+                Intent intent = new Intent(mContext, MainActivity.class);
+                intent.putExtra("입력아이디", inputId);
+
+//                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+
+                finish();
+            }
+        });
+
         signUpTxt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -39,6 +64,7 @@ public class LoginActivity extends BaseActivity {
     @Override
     public void bindViews() {
         signUpTxt = findViewById(R.id.signUpTxt);
-
+        loginBtn = findViewById(R.id.loginBtn);
+        idEdt = findViewById(R.id.idEdt);
     }
 }
---
+++ app/src/main/java/kr/tjeit/a20181216_02_loginandsignup/MainActivity.java
@@ -0,0 +1,39 @@
+package kr.tjeit.a20181216_02_loginandsignup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends BaseActivity {
+
+    TextView userIdTxt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bindViews();
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+        String userid = getIntent().getStringExtra("입력아이디");
+
+        userIdTxt.setText(userid);
+    }
+
+    @Override
+    public void bindViews() {
+        userIdTxt = findViewById(R.id.userIdTxt);
+
+    }
+}
