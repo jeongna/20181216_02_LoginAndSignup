7ebc3491a76a8caa8096b06f3179c21ad8097198
ksh1101 <0solarboy0@naver.com>
2018-12-16 14:46:55 +0900

    약관 동의 화면 그리기

--- app/src/main/java/kr/tjeit/a20181216_02_loginandsignup/LoginActivity.java
+++ app/src/main/java/kr/tjeit/a20181216_02_loginandsignup/LoginActivity.java
@@ -1,9 +1,14 @@
 package kr.tjeit.a20181216_02_loginandsignup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends BaseActivity {
+
+    TextView signUpTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +22,13 @@ public class LoginActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
-
+        signUpTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -27,6 +38,7 @@ public class LoginActivity extends BaseActivity {
 
     @Override
     public void bindViews() {
+        signUpTxt = findViewById(R.id.signUpTxt);
 
     }
 }
