9078b0a68f35d5cafa8bd467d353802ed7007252
ksh1101 <0solarboy0@naver.com>
2018-12-16 15:06:53 +0900

    약관 동의 화면 기능 마무리

--- app/src/main/java/kr/tjeit/a20181216_02_loginandsignup/SignUpActivity.java
+++ app/src/main/java/kr/tjeit/a20181216_02_loginandsignup/SignUpActivity.java
@@ -1,9 +1,22 @@
 package kr.tjeit.a20181216_02_loginandsignup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
 public class SignUpActivity extends BaseActivity {
+
+    CheckBox allCheckBox;
+    CheckBox moneyCheckBox;
+    CheckBox agreementCheckBox;
+    
+    Button nextBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +30,44 @@ public class SignUpActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+        
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(allCheckBox.isChecked() && moneyCheckBox.isChecked() && agreementCheckBox.isChecked()){
+//                    다음 화면으로 이동해야함
+                    Log.d("약관동의","모두 동의함. 다음화면으로");
+
+                    Intent intent = new Intent(mContext, SignUpStep2Activity.class);
+                    startActivity(intent);
+                }
+                else {
+//                    사용자에게 모두 동의하셔야 가입이 가능합니다. 안내메세지
+//                    Toast 를 이용
+                    Toast.makeText(mContext, "모두 동의하셔야 가입이 가능합니다.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        
+        allCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+//                isChecked : true => 눌리는 상황
+//                false => 해제되는 상황
+
+                if(isChecked){
+//                    전체 동의가 눌렸다면? 금융/이용 동의 처리
+                    moneyCheckBox.setChecked(true);
+                    agreementCheckBox.setChecked(true);
+                }
+                else {
+                    moneyCheckBox.setChecked(false);
+                    agreementCheckBox.setChecked(false);
+                }
+
+            }
+        });
 
     }
 
@@ -27,6 +78,10 @@ public class SignUpActivity extends BaseActivity {
 
     @Override
     public void bindViews() {
+        allCheckBox = findViewById(R.id.allCheckBox);
+        moneyCheckBox = findViewById(R.id.moneyCheckBox);
+        agreementCheckBox = findViewById(R.id.agreementCheckBox);
+        nextBtn = findViewById(R.id.nextBtn);
 
     }
 }
